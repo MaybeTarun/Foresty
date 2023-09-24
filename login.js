ea2834af93439260d45b21db3145a764b50c395f
Tarun Gupta <90061716+MaybeTarun@users.noreply.github.com>
2023-09-24 08:43:40 +0530

    added a lot of things

--- login.js
+++ login.js
@@ -1,3 +1,8 @@
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+// import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
 const bg = document.getElementById('bg');
 const num = 400;
 
@@ -29,24 +34,17 @@ document.querySelector("h1").onmouseover = event => {
     }, 50);
 };
 
-document.getElementById("login").addEventListener("click", function (e) {
-    e.preventDefault();
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBGhgjHNKQhiBmQA2hXlhckfrOYXcdakl8",
+//   authDomain: "foresty-7bb83.firebaseapp.com",
+//   projectId: "foresty-7bb83",
+//   storageBucket: "foresty-7bb83.appspot.com",
+//   messagingSenderId: "220063895814",
+//   appId: "1:220063895814:web:c026263c32a47ab8518e87"
+// };
 
-    var lemail = document.getElementById("lemail").value;
-    var lpassword = document.getElementById("lpassword").value;
-
-    if (name === "" || email === "" || password === "") {
-        alert("Please fill in all fields.");
-        return;
-    }
-
-    console.log("Email: " + lemail);
-    console.log("Password: " + lpassword);
-
-    document.getElementById("lemail").value = "";
-    document.getElementById("lpassword").value = "";
-
-});
+// const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
 
 document.getElementById("register").addEventListener("click", function (e) {
     e.preventDefault();
@@ -60,12 +58,54 @@ document.getElementById("register").addEventListener("click", function (e) {
         return;
     }
 
-    console.log("Name: " + name);
-    console.log("Email: " + email);
-    console.log("Password: " + password);
+    // createUserWithEmailAndPassword(auth, email, password)
+    // .then((userCredential) => {
+    //     const user = userCredential.user;
+    //     console.log("Account Created");
+    // })
+    // .catch((error) => {
+    //     const errorCode = error.code;
+    //     const errorMessage = error.message;
+    //     console.log(errorCode, errorMessage);
+    // });
 
-    document.getElementById("name").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("password").value = "";
+    console.log(name, email, password);
+
+    // document.getElementById("name").value = "";
+    // document.getElementById("email").value = "";
+    // document.getElementById("password").value = "";
+
+});
+
+document.getElementById("login").addEventListener("click", function (e) {
+    e.preventDefault();
+
+    var lemail = document.getElementById("lemail").value;
+    var lpassword = document.getElementById("lpassword").value;
+
+    if (lemail === "" || lpassword === "") {
+        alert("Please fill in all fields.");
+        return;
+    }
+
+    // signInWithEmailAndPassword(auth, email, password)
+    // .then((userCredential) => {
+    //     const user = userCredential.user;
+    //     window.open('www.google.com');
+    //     console.log("Successful");
+
+    // })
+    // .catch((error) => {
+    //     const errorCode = error.code;
+    //     const errorMessage = error.message;
+    //     console.log(errorCode, errorMessage);
+    // });
+
+    console.log(lemail, lpassword);
+    var htmlFilePath = 'main.html';
+    window.location.href = htmlFilePath;
+
+    // document.getElementById("lemail").value = "";
+    // document.getElementById("lpassword").value = "";
 
 });
